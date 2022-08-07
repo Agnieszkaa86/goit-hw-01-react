e597f0815be529936c3177b8528aaf56e3cb4a9b
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-08-07 21:07:46 +0200

    add transaction

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,61 +1,62 @@
 import React from 'react';
-//import styled from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-// const Table = styled.table`
-//   display: block;
-//   width: 870px;
-//   margin: 0;
-//   padding: 0;
-//   margin-bottom: 20px;
-//   border: 1px solid #e0e6e7;
-//   border-collapse: collapse;
-//   color: #686461;
-//   box-shadow: 2px 2px 2px 3px rgba(0, 0, 0, 0.25);
-// `;
-// const Thead = styled.thead`
-//   background: #00bcd4;
-//   color: white;
-//   padding: 10px 20px;
-//   font-family: 'Nobile', sans-serif;
-//   text-transform: uppercase;
-//   font-size: 13px;
-//   font-weight: 700;
-// }
-// `;
-// const Trow = styled.tr`
-//   text-transform: capitalize;
-//   text-align: center;
-//   padding-left: 114px;
-//   &:nth-child(even) {
-//     background-color: #ecf2f3;
-//   }
-// `;
+const Table = styled.table`
+ 
+  display: block;
+  width: 500px;
+  margin: 0 auto;
+  padding: 0;
+  margin-bottom: 20px;
+  border: 1px solid #e0e6e7;
+  color: #686461;
+  
+`;
+const Th = styled.th`
+  width: 500px;
+  background: #00bcd4;
+  color: white;
+  padding: 10px 20px;
+  font-family: 'Nobile', sans-serif;
+  text-transform: uppercase;
+  font-size: 13px;
+  font-weight: 700;
+`;
+
+const Tr = styled.tr`
+  text-transform: capitalize;
+  text-align: center;
+  padding-left: 114px;
+   &:nth-child(even) {
+     background-color: #ecf2f3;
+   }
+ `;
 
 function TransactionHistory(props) {
-  const { history } = props;
+  const {items} = props;
   return (
-    <div>
-      <table>
+    
+      <Table>
         <thead>
           <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+            <Th>Type</Th>
+            <Th>Amount</Th>
+            <Th>Currency</Th>
           </tr>
         </thead>
 
         <tbody>
-          {history.map(item => (
-            <tr key={item.id}>
+          {items.map(item => (
+            <Tr key={item.id}>
               <td>{item.type}</td>
               <td>{item.amount}</td>
               <td>{item.currency}</td>
-            </tr>
+            </Tr>
           ))}
         </tbody>
-      </table>
-    </div>
+      </Table>
+    
   );
 }
 TransactionHistory.prototypes = {
