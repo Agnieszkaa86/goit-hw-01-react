1bd1d66692d92f83a6702bc00235efb11a824a91
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-08-07 19:00:57 +0200

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import FriendList from './FriendList';
+import friends from './data/friends.json';
+import Profile from './Profile';
+import user from './data/user.json';
+import Statistics from './Statistics';
+import data from './data/data.json';
+import TransactionHistory from './TransactionHistory';
+import transactions from './data/transactions';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <FriendList friends={friends} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
